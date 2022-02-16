a332aebebffa39ccee4e33d6199eef4f2a8a48f5
Sara894 <t48250klo@gmail.com>
2022-02-16 21:29:10 +0300

    переход к функциональному или декларативному программ

--- index.php
+++ index.php
@@ -1,17 +1,27 @@
 <?php
-$pdo = new PDO("mysql:host=localhost; dbname=tech;","root","");
-$sql = "SELECT * FROM tasks";
-$statement = $pdo->prepare($sql);
-$statement->execute();
-$tasks = $statement->fetchAll(PDO::FETCH_OBJ);
+
+
+function getAllTasks()
+{
+    //подключение к бд
+    $pdo = new PDO("mysql:host=localhost; dbname=tech;", "root", "");
+    //запрос к бд
+    $sql = "SELECT * FROM tasks";
+    $statement = $pdo->prepare($sql);//подготовили запрос к бд
+    $statement->execute();//отправка запроса к бд
+    $tasks = $statement->fetchAll(PDO::FETCH_OBJ);//получаем все записи из таблицы
+    return $tasks;
+}
 ?>
 
 <!docktype html>
-<html lang="ru">
+    <html lang="ru">
+
     <head>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
 
     </head>
+
     <body>
         <div class="container">
             <div class="row">
@@ -27,20 +37,20 @@ $tasks = $statement->fetchAll(PDO::FETCH_OBJ);
                             </tr>
                         </thead>
                         <tbody>
-                            <?php foreach($tasks as $task):?>
-                        <tr>
-                                <td><?=$task->id?></td>
-                                <td><?=$task->title?></td>
-                                <td>
-                                    <a href="/show.php?id=<?=$task->id?>" class="btn btn-primary">Show</a>
-                                    <a href="/edit.php?id=<?=$task->id?>" class="btn btn-warning">Edit</a>
-                                    <a href="/delete.php?id=<?=$task->id?>" class="btn btn-danger">Delete</a>
-                                </td>
-                            </tr>
-                            <?endforeach;?>
+                            <?php foreach ($tasks as $task) : ?>
+                                <tr>
+                                    <td><?= $task->id ?></td>
+                                    <td><?= $task->title ?></td>
+                                    <td>
+                                        <a href="/show.php?id=<?= $task->id ?>" class="btn btn-primary">Show</a>
+                                        <a href="/edit.php?id=<?= $task->id ?>" class="btn btn-warning">Edit</a>
+                                        <a href="/delete.php?id=<?= $task->id ?>" class="btn btn-danger">Delete</a>
+                                    </td>
+                                </tr>
+                            <? endforeach; ?>
                         </tbody>
                     </table>
-</div>
-</div>
+                </div>
+            </div>
         </div>
     </body>
